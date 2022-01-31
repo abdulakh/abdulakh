68cb8389472214c5b3415076fb0658cbb382b6bd
abdulakh <magomedovabdullah20042007@mail.ru>
2022-01-31 19:03:40 +0300

    Commit Mesage

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,101 @@
+// // import 'package:flutter/material.dart';
+// //
+// // void main() {
+// // runApp(
+// //   Container(
+// //     color: Colors.teal,
+// //     padding: EdgeInsets.only(top:100),
+// //     alignment: Alignment.center,
+// //     // child: Column(
+// //     //   children: [
+// //     //     Container(
+// //     //       color: Colors.red,
+// //     //       constraints: BoxConstraints.tightFor(width: 300),
+// //     //       child: const Text('Как сделать',
+// //     //               textDirection: TextDirection.ltr,
+// //     //               style: TextStyle(fontSize: 26)
+// //     //           ),
+// //     //     ),
+// //     //     Container(
+// //     //       color: Colors.orange,
+// //     //       constraints: BoxConstraints.tightFor(width: 300),
+// //     //       child: const Text('Как сделать',
+// //     //           textDirection: TextDirection.ltr,
+// //     //           style: TextStyle(fontSize: 26)
+// //     //       ),
+// //     //     ),
+// //     //     Container(
+// //     //       color: Colors.yellow,
+// //     //       constraints: BoxConstraints.tightFor(width: 300),
+// //     //       child: const Text('Как сделать',
+// //     //           textDirection: TextDirection.ltr,
+// //     //           style: TextStyle(fontSize: 26)
+// //     //       ),
+// //     //     ),
+// //     //
+// //     //     // Row(
+// //     //     //   children: [
+// //     //     //     Container(
+// //     //     //       color: Colors.yellow,
+// //     //     //       child: const Text('Как сделать',
+// //     //     //           textDirection: TextDirection.ltr,
+// //     //     //           style: TextStyle(fontSize: 26)
+// //     //     //       ),
+// //     //     //     ),
+// //     //     //     Container(
+// //     //     //       color: Colors.orange,
+// //     //     //       child: const Text('Как сделать',
+// //     //     //           textDirection: TextDirection.ltr,
+// //     //     //           style: TextStyle(fontSize: 26)
+// //     //     //       ),
+// //     //     //     ),
+// //     //     //     Container(
+// //     //     //       color: Colors.red,
+// //     //     //       child: const Text('Как сделать',
+// //     //     //           textDirection: TextDirection.ltr,
+// //     //     //           style: TextStyle(fontSize: 26)
+// //     //     //       ),
+// //     //     //     ),
+// //     //     //   ],
+// //     //     // ),
+// //     //   ],
+// //     // ),
+// //       ),
+// //   );
+// // }
 // import 'package:flutter/material.dart';
 //
 // void main() {
-// runApp(
-//   Container(
-//     color: Colors.teal,
-//     padding: EdgeInsets.only(top:100),
-//     alignment: Alignment.center,
-//     // child: Column(
-//     //   children: [
-//     //     Container(
-//     //       color: Colors.red,
-//     //       constraints: BoxConstraints.tightFor(width: 300),
-//     //       child: const Text('Как сделать',
-//     //               textDirection: TextDirection.ltr,
-//     //               style: TextStyle(fontSize: 26)
-//     //           ),
-//     //     ),
-//     //     Container(
-//     //       color: Colors.orange,
-//     //       constraints: BoxConstraints.tightFor(width: 300),
-//     //       child: const Text('Как сделать',
-//     //           textDirection: TextDirection.ltr,
-//     //           style: TextStyle(fontSize: 26)
-//     //       ),
-//     //     ),
-//     //     Container(
-//     //       color: Colors.yellow,
-//     //       constraints: BoxConstraints.tightFor(width: 300),
-//     //       child: const Text('Как сделать',
-//     //           textDirection: TextDirection.ltr,
-//     //           style: TextStyle(fontSize: 26)
-//     //       ),
-//     //     ),
-//     //
-//     //     // Row(
-//     //     //   children: [
-//     //     //     Container(
-//     //     //       color: Colors.yellow,
-//     //     //       child: const Text('Как сделать',
-//     //     //           textDirection: TextDirection.ltr,
-//     //     //           style: TextStyle(fontSize: 26)
-//     //     //       ),
-//     //     //     ),
-//     //     //     Container(
-//     //     //       color: Colors.orange,
-//     //     //       child: const Text('Как сделать',
-//     //     //           textDirection: TextDirection.ltr,
-//     //     //           style: TextStyle(fontSize: 26)
-//     //     //       ),
-//     //     //     ),
-//     //     //     Container(
-//     //     //       color: Colors.red,
-//     //     //       child: const Text('Как сделать',
-//     //     //           textDirection: TextDirection.ltr,
-//     //     //           style: TextStyle(fontSize: 26)
-//     //     //       ),
-//     //     //     ),
-//     //     //   ],
-//     //     // ),
-//     //   ],
-//     // ),
-//       ),
+//   runApp(Container(
+//       padding: EdgeInsets.all(30),
+//       color: Colors.teal,
+//       child:Row(
+//         textDirection: TextDirection.ltr,
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: <Widget>[
+//           Text('Tom',
+//               textDirection: TextDirection.ltr),
+//           Text('Bob',
+//               textDirection: TextDirection.ltr),
+//           Text('Sam',
+//               textDirection: TextDirection.ltr),
+//           Text('Alice',
+//               textDirection: TextDirection.ltr)
+//         ],
+//       )
+//   )
 //   );
 // }
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Container(
-      padding: EdgeInsets.all(30),
-      color: Colors.teal,
-      child:Row(
-        textDirection: TextDirection.ltr,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Text('Tom',
-              textDirection: TextDirection.ltr),
-          Text('Bob',
-              textDirection: TextDirection.ltr),
-          Text('Sam',
-              textDirection: TextDirection.ltr),
-          Text('Alice',
-              textDirection: TextDirection.ltr)
-        ],
-      )
-  )
+void main(){
+  runApp(
+    Center(
+      child: Container(
+        padding: EdgeInsets.all(30),
+        color:Colors.teal,
+        child: Icon(Icons.mobile_screen_share,size: 100,),
+      ),
+    ),
   );
 }
